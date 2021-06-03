63f7215efbe3175b6bb0d615ab478d1dc81fcb90
nguyenduchuy71 <wtfomg3650@gmail.com>
2021-06-03 08:55:46 +0700

    new fe

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,92 +1,137 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import MenuIcon from "@material-ui/icons/Menu";
 import CloseIcon from "@material-ui/icons/Close";
 import { Link } from "react-router-dom";
-import { useSelector,useDispatch } from "react-redux";
-import MenuItem from '@material-ui/core/MenuItem';
-import FormControl from '@material-ui/core/FormControl';
-import Select from '@material-ui/core/Select';
+import { useSelector } from "react-redux";
+import MenuItem from "@material-ui/core/MenuItem";
 import Cookie from "js-cookie";
-
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
+import HomeIcon from "@material-ui/icons/Home";
+import ForumIcon from "@material-ui/icons/Forum";
+import MenuBookIcon from "@material-ui/icons/MenuBook";
 function Header() {
   const [burgerState, setBurgerState] = useState(false);
+  const [openSubMenu, setopenSubMenu] = useState(false);
   const userSignin = useSelector((state) => state.userSignin);
   let { loading, userInfo, error } = userSignin;
-  const dispatch = useDispatch();
-  useEffect(() => {
-  }, [userInfo]);
-  const signOut =()=>{
-    Cookie.remove('userInfo');
+  useEffect(() => {}, [userInfo]);
+  const signOut = () => {
+    Cookie.remove("userInfo");
     window.location.reload();
-  }
+  };
+  const toogleSubMenu = () => {
+    setopenSubMenu(!openSubMenu);
+  };
   return (
-    <Container>
-      <a>
-        <img src="/images/logo.png" alt="logo" />
-      </a>
-      <Menu>
-        <Link to="/">Trang Chủ</Link>
-        <Link to="/course">Khóa học</Link>
-        <Link to="/forum">Tương tác</Link>
-      </Menu>
-      <RightMenu>
-        { userInfo? 
-              <ContainerSubNav>
-                <img src={userInfo.avatar} alt="avatar"/>
-                <FormControl>
-                  <Select>
-                    <MenuItem>Tài khoản</MenuItem>
-                    <MenuItem onClick={signOut}>Đăng xuất</MenuItem>
-                  </Select>
-                </FormControl>
-              </ContainerSubNav>
-              :<Link to="/signin">Đăng nhập</Link>
-        }
-      </RightMenu>
-      <ContainerCustomeMenu>
-        <CustomMenu onClick={() => setBurgerState(!burgerState)}></CustomMenu>
-      </ContainerCustomeMenu>
-      <BurgerNav show={burgerState}>
-        <CloseWrapper>
-          <CustomeClose onClick={() => setBurgerState(false)} />
-        </CloseWrapper>
-        <li>
+    <Content>
+      <Container>
+        <a>
+          <img src="/images/logo.png" alt="logo" />
+        </a>
+        <Menu>
           <Link to="/">Trang Chủ</Link>
-        </li>
-        <li>
           <Link to="/course">Khóa học</Link>
-        </li>
-        <li>
           <Link to="/forum">Tương tác</Link>
-        </li>
-        <li>
-          { userInfo? 
-              <>
-                <MenuItem><Link to="/me">Tài khoản</Link></MenuItem>
-                <MenuItem onClick={signOut}>Đăng xuất</MenuItem>
-              </>:
-              <Link to="/signin">Đăng nhập</Link>
-          }
-        </li>
-      </BurgerNav>
-    </Container>
+        </Menu>
+        <RightMenu>
+          {userInfo ? (
+            <SelectMenu onClick={toogleSubMenu}>
+              <img src={userInfo.avatar} alt="avatar" />
+              <ArrowContainer className="arrowcontainer">
+                <img src="/images/arrow.png" alt="arrow" />
+              </ArrowContainer>
+            </SelectMenu>
+          ) : (
+            <Link to="/signin">Đăng nhập</Link>
+          )}
+        </RightMenu>
+        <ContainerCustomeMenu>
+          <CustomMenu onClick={() => setBurgerState(!burgerState)}></CustomMenu>
+        </ContainerCustomeMenu>
+        <BurgerNav show={burgerState}>
+          <CloseWrapper>
+            <CustomeClose onClick={() => setBurgerState(false)} />
+          </CloseWrapper>
+          <ElementMenu>
+            <HomeIcon />
+            <div>
+              <Link to="/">Trang Chủ</Link>
+            </div>
+          </ElementMenu>
+          <ElementMenu>
+            <MenuBookIcon />
+            <div>
+              <Link to="/course">Khóa học</Link>
+            </div>
+          </ElementMenu>
+          <ElementMenu>
+            <ForumIcon />
+            <div>
+              <Link to="/forum">Tương tác</Link>
+            </div>
+          </ElementMenu>
+          {userInfo ? (
+            <div>
+              <ElementMenu>
+                <AccountCircleIcon />
+                <div>
+                  <Link to="/">Tài khoản</Link>
+                </div>
+              </ElementMenu>
+              <ElementMenu>
+                <ExitToAppIcon />
+                <div>
+                  <Link onClick={signOut}>Đăng xuất</Link>
+                </div>
+              </ElementMenu>
+            </div>
+          ) : (
+            <Link to="/signin">Đăng nhập</Link>
+          )}
+        </BurgerNav>
+      </Container>
+      {openSubMenu ? (
+        <SubMenu className="sub_menu">
+          <ListSubMenu>
+            <ElementSubMenu>
+              <AccountCircleIcon />
+              <div>
+                <Link to="/">Tài khoản</Link>
+              </div>
+            </ElementSubMenu>
+            <ElementSubMenu>
+              <ExitToAppIcon />
+              <div>
+                <Link onClick={signOut}>Đăng xuất</Link>
+              </div>
+            </ElementSubMenu>
+          </ListSubMenu>
+        </SubMenu>
+      ) : (
+        <></>
+      )}
+    </Content>
   );
 }
 
 export default Header;
+const Content = styled.div`
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+`;
 const Container = styled.div`
   min-height: 60px;
-  position: fixed;
+  width: 100%;
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: #ccc;
   padding: 0 20px;
-  top: 0;
-  left: 0;
-  right: 0;
-  z-index: 1;
   a img {
     width: 68px;
     height: 68px;
@@ -118,9 +163,9 @@ const RightMenu = styled.div`
     text-transform: uppercase;
     margin-right: 10px;
   }
-  img{
-    width:44px;
-    height:44px;
+  img {
+    width: 44px;
+    height: 44px;
     cursor: pointer;
   }
   @media (max-width: 600px) {
@@ -151,14 +196,6 @@ const BurgerNav = styled.div`
   text-align: start;
   transform: ${(props) => (props.show ? "translateX(0)" : "translateX(100%)")};
   transition: transform 0.25s;
-  li {
-    padding: 15px 0;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.2);
-
-    a {
-      font-weight: 600;
-    }
-  }
 `;
 const CustomeClose = styled(CloseIcon)`
   cursor: pointer;
@@ -167,4 +204,51 @@ const CloseWrapper = styled.div`
   display: flex;
   justify-content: flex-end;
 `;
-const ContainerSubNav = styled.div``;
+const SubMenu = styled.div`
+  position: absolute;
+  top: 80%;
+  right: 10px;
+  bottom: 0;
+  border-radius: 4px;
+`;
+const ListSubMenu = styled.ul`
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  background-color: #fff;
+`;
+const ElementSubMenu = styled.li`
+  margin: 10px 4px;
+  display: flex;
+  align-items: center;
+  padding: 0px 10px;
+  &:hover {
+    background-color: #ccc;
+  }
+  div {
+    margin-left: 10px;
+  }
+`;
+const SelectMenu = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+const ArrowContainer = styled.div`
+  position: relative;
+  img {
+    width: 12px;
+    height: 12px;
+    margin-top: 30px;
+    margin-left: 4px;
+  }
+`;
+const ElementMenu = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.2);
+  padding: 20px 0;
+  div {
+    margin-left: 10px;
+  }
+`;
